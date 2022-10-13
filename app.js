3416accd0ea271a25ad7ae7ed92f30504e2776d6
Louis Gadza <siuolgadza@gmail.com>
2022-10-13 17:20:05 +0200

    updates

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ window.onload = function () {
 
 const createCells = function () {
   const cells = document.getElementById("cells");
-  for (let i = 0; i < 72; i++) {
+  for (let i = 0; i < 76; i++) {
     const cell = document.createElement("div");
     cell.className = "cell";
     const h3 = document.createElement("h3");
@@ -12,5 +12,20 @@ const createCells = function () {
     cell.appendChild(h3);
     cells.appendChild(cell);
   }
+  // const cell = document.querySelectorAll("div")[1];
+  // const cell = document.querySelectorAll("h3");
+  // return cell;
 };
-const randomNum=Math.
+
+const randomNum = Math.floor(Math.random() * 76 + 1);
+const randomCell = document.querySelector("#myBtn");
+console.log(randomNum);
+randomCell.onclick = function () {
+  const h3 = createCells();
+  for (let i = 0; i < h3.length; i++) {
+    const block = h3.innerText;
+    if (randomNum === block) {
+      cell.style.backgroudColor = "lightblue";
+    }
+  }
+};
